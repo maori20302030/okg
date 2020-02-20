cfa1e139ec4b09669fa35d2696923994d465421c
maori <maori-2010@hotmail.com>
2020-02-20 14:04:50 -0500

    Initial Commit

---
+++ okg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  okg
+//
+//  Created by Omar Algrimeel on 2/20/20.
+//  Copyright © 2020 Maori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
